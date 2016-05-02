84632422ffa8f1dcb2659f8aa40dd65119d7d650
Patient <654444897@qq.com>
2016-05-02 17:07:15 +0800

    567689

--- LittleGaEnglish/Class/IM/liaotianjiemian.m
+++ LittleGaEnglish/Class/IM/liaotianjiemian.m
@@ -9,6 +9,23 @@
 #import "liaotianjiemian.h"
 #import "reactlabel.h"
 
+@interface liaotianyuyinzuo : UITableViewCell
+
+@end
+@implementation liaotianyuyinzuo
+
+
+@end
+
+@interface liaotianyuyinyou : UITableViewCell
+
+@end
+@implementation liaotianyuyinyou
+
+
+@end
+
+
 @interface liaotianjiezuo : UITableViewCell
 @property (strong, nonatomic) IBOutlet UIImageView *imagecover;
 @property (strong, nonatomic) IBOutlet reactlabel *lablezuo;
@@ -27,7 +44,6 @@
     CGSize maxSize = CGSizeMake(MAXFLOAT,32);
     NSAttributedString *content = [[NSAttributedString alloc] initWithString:comment attributes:@{NSFontAttributeName: [UIFont systemFontOfSize:14.]}];
     CGRect contentBoundingRect = [content boundingRectWithSize:maxSize options:NSStringDrawingTruncatesLastVisibleLine | NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading context: nil];
-    NSLog(@"%f",contentBoundingRect.size.width);
     return contentBoundingRect.size.width + 16;
 }
 @end
@@ -46,13 +62,24 @@
 
 @interface liaotianjiemian ()<UITableViewDelegate,UITableViewDataSource>
 @property (strong, nonatomic) IBOutlet UITableView *tableview;
+@property (strong, nonatomic) IBOutlet UITextField *uitextfile;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *bottomlayout;
 
+@property (strong, nonatomic) IBOutlet UIImageView *长按手势;
+@property (strong, nonatomic) NSMutableArray *arrtest;
+@property (assign, nonatomic) BOOL hidebool;
 @end
 
 @implementation liaotianjiemian
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.arrtest = [NSMutableArray arrayWithCapacity:0];
+    [self.arrtest addObjectsFromArray:@[@"assas",@"dds",@"dsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsds",@"assas",@"assas",@"assas"]];
+    self.hidebool = true;
+    UILongPressGestureRecognizer * longPressGr = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longPressToDo:)];
+    longPressGr.minimumPressDuration = 1.0;
+    [self.长按手势 addGestureRecognizer:longPressGr];
     // Do any additional setup after loading the view.
 }
 
@@ -60,20 +87,29 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return self.arrtest.count;
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    if (indexPath.row == 0) {
+        liaotianyuyinzuo *cell = [tableView dequeueReusableCellWithIdentifier:@"liaotianyuyinzuo" forIndexPath:indexPath];
+        return cell;
+    }
+    if (indexPath.row == 1) {
+        liaotianyuyinyou *cell = [tableView dequeueReusableCellWithIdentifier:@"liaotianyuyinyou" forIndexPath:indexPath];
+        return cell;
+    }
     if (indexPath.row == 2) {
         liaotianjieyou *cell = [tableView dequeueReusableCellWithIdentifier:@"liaotianjieyou" forIndexPath:indexPath];
-        cell.labelyou.text = @"dsddsdsdsdsdds";
+        cell.labelyou.text = self.arrtest[indexPath.row];
         cell.wight.constant = [self cellwightcellstr:cell.labelyou.text];
         return cell;
     }
     liaotianjiezuo *cell = [tableView dequeueReusableCellWithIdentifier:@"liaotianjiezuo" forIndexPath:indexPath];
-    cell.lablezuo.text = @"dsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsds";
+    cell.lablezuo.text = self.arrtest[indexPath.row];
     cell.wight.constant = [self cellwightcellstr:cell.lablezuo.text];
     return cell;
 }
@@ -90,10 +126,36 @@
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.row == 2) {
-        return [liaotianjiezuo tableheightForContent:@"dsddsdsdsdsdds" fixSize:self.tableview.frame.size];
+        return [liaotianjiezuo tableheightForContent:self.arrtest[indexPath.row] fixSize:self.tableview.frame.size];
     }
-    return [liaotianjiezuo tableheightForContent:@"dsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsdsdsddsdsds" fixSize:self.tableview.frame.size];
+    return [liaotianjiezuo tableheightForContent:self.arrtest[indexPath.row] fixSize:self.tableview.frame.size];
 }
+- (IBAction)fasonganniu:(id)sender {
+#warning 发送按钮
+    [self.arrtest addObject:self.uitextfile.text];
+    [self.tableview reloadData];
+    [self.tableview scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.arrtest.count - 1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    self.uitextfile.text = nil;
+}
+- (IBAction)tapyuyin:(id)sender {
+//    [self keyboardDidShow];
+    [self.uitextfile resignFirstResponder];
+
+    if (self.hidebool) {
+    self.bottomlayout.constant = 120;
+    [self.tableview scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:self.arrtest.count - 1 inSection:0] atScrollPosition:UITableViewScrollPositionBottom animated:YES];
+    self.hidebool = false;
+        return;
+    }
+    self.bottomlayout.constant = 0;
+    self.hidebool = true;
+}
+-(void)longPressToDo:(UILongPressGestureRecognizer *)gesture
+{
+    NSLog(@"长按手势");
+
+}
+
 /*
 #pragma mark - Navigation
 
--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
-   pod 'UAProgressView', '~> 0.1.3'
+pod 'UAProgressView'
+
 platform:ios,'7.0'
-pod 'JSONModel', '~> 1.0.1'
+pod 'JSONModel’
 pod 'Masonry'
 pod 'AFNetworking', '~> 2.5.1'
 pod 'SDWebImage', '~> 3.7.1'
@@ -8,6 +9,7 @@ pod 'MJRefresh'
 pod 'THSegmentedPager', '~> 1.1.3'
 pod 'ReactiveCocoa', '~> 2.5'
 pod 'SVProgressHUD'
+pod 'IQKeyboardManager'
 
 
 
